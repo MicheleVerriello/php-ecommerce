91b83739215af59939bf1f8265557aa7718e0159
Michele <micverriello@gmail.com>
2024-02-01 19:12:59 +0100

    feat(api): add api routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// authorization
+Route::post('/api/login', 'AuthController@login');
+Route::post('/api/signup', 'AuthController@signup');
+
+// items
+Route::post('/api/items', 'ItemController@insertItem');
+Route::put('/api/items/{id}', 'ItemController@updateItem');
+Route::delete('/api/items/{id}', 'ItemController@deleteItem');
+Route::get('/api/items/{id}', 'ItemController@getItem');
+Route::get('/api/items', 'ItemController@getItems');
+
+// cart
+Route::post('/api/carts/{id}/items', 'CartController@insertItemIntoCart');
+Route::put('/api/carts/items/{idCartItem}', 'CartController@updateCartItemQuantity');
+Route::delete('/api/carts/items/{idCartItem}', 'CartController@deleteCartItem');
+Route::get('/api/carts/{idUser}', 'CartController@insertItem');
+
+// orders
+Route::post('/api/orders', 'OrderController@insertOrder');
+Route::get('/api/orders/{idUser}', 'OrderController@getOrdersByUserId');
+Route::get('/api/orders/{id}', 'OrderController@getOrderDetails');
+Route::delete('/api/orders/{id}', 'OrderController@deleteOrder');
+
+
+
